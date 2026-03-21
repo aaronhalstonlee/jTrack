f6ac55b6f7fb1091abc9a369d72d66efd9eb3d75
aaron <33557557+aaronhalstonlee@users.noreply.github.com>
2026-03-21 16:52:55 -0700

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,11 @@
+const cacheName = 'jtrack-v1';
+const assets = ['./', './index.html', './style.css', './script.js', './manifest.json', './click.mp3', './success.mp3', './icon.png'];
+
+self.addEventListener('install', e => {
+    e.waitUntil(caches.open(cacheName).then(cache => cache.addAll(assets)));
+});
+
+self.addEventListener('fetch', e => {
+    e.respondWith(caches.match(e.request).then(res => res || fetch(e.request)));
+})
+  ;
